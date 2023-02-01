215cf2c42725760cab263e6fffc8e7e617d2ae91
Aaryan Sharma <aaryan@Aaryans-MBP.Dlink>
2023-02-01 15:09:35 +0530

    Backend server added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -22,12 +22,15 @@ const Home = () => {
     const fetchPosts = async () => {
       setLoading(true);
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json", // no body as its GET
-          },
-        });
+        const response = await fetch(
+          "https://sikebot.onrender.com/api/v1/post",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json", // no body as its GET
+            },
+          }
+        );
         if (response.ok) {
           const result = await response.json();
           setAllPosts(result?.data.reverse());
